f5318313156add613d53a9b8378062fe26b27806
ZazaSaki <isac.cruzm@gmail.com>
2024-03-22 13:08:45 +0000

    reading multiple samples from adc

--- ACII/Aula 6/prog1.c
+++ ACII/Aula 6/prog1.c
@@ -1,6 +1,8 @@
 #include <detpic32.h>
 #include "../outils.c"
 
+#define SAMPLES 16
+
 int main(int argc, char const *argv[])
 {
     // TRISBbits.TRISB4 = 1;
@@ -19,7 +21,7 @@ int main(int argc, char const *argv[])
     // interrupt is generated. At the same time,
     // hardware clears the ASAM bit
     AD1CON3bits.SAMC = 16; // Sample time is 16 TAD (TAD = 100 ns)
-    AD1CON2bits.SMPI = 4-1; // Interrupt is generated after N samples
+    AD1CON2bits.SMPI = SAMPLES-1; // Interrupt is generated after N samples
     // (replace N by the desired number of
     // consecutive samples)
     AD1CHSbits.CH0SA = 4; // replace 4 by the desired input
@@ -36,20 +38,65 @@ int main(int argc, char const *argv[])
     TRISDbits.TRISD5 = 0;
     TRISDbits.TRISD6 = 0;
     LATE = 0X0000;
-
+    int c = 0;
+    int i = 0;
+    int accumulator = 0;
+    int res = 33;
     while (1)
     {
         AD1CON1bits.ASAM = 1; 
-        while(IFS1bits.AD1IF == 0);
+        if (PORTDbits.RD8 == 0)
+        {
+            c=0;
+        }
+        
+        while(IFS1bits.AD1IF == 0){
             
-        IFS1bits.AD1IF = 0;
+
+            if (PORTDbits.RD8 != 0 & c <84)
+            {
+                c++;
+            }
+        };
+            
         
-        printInt(ADC1BUF0, 16 | 3 << 16) ;
+        
+        printInt(c, 10 | 3 << 10) ;
+
+        putChar('_') ;
+        printInt(PORTDbits.RD8, 10 | 3 << 10) ;
+        
+
+        int *p = (int *)(&ADC1BUF0);
+        for(; p <= (int *)(&ADC1BUF0 + SAMPLES*4); p+=4 ) {
+            putChar('_');
+            printInt(*p, 10 | 3 << 10) ;
+
+            accumulator += *p;
+        }    
+
+
+        accumulator = (accumulator/SAMPLES+512)*res/1024;
+
+        putChar('_') ;
+        putChar('_') ;
+        putChar('_') ;
+        printInt(accumulator, 10 | 3 << 10) ;
+
+        putChar('_') ;
+        putChar('_') ;
+        putChar('_') ;
+        
         putChar('\r');
-        
-        int i = ADC1BUF0;
-        delay(i);
-        sendToDisp10(45,i);
+        putChar('\r');
+        putChar('\r');
+        putChar('\r');
+
+        IFS1bits.AD1IF = 0;
+        //delay(i);
+        //sendToDisp10(45,i);
+
+
 
 
         
