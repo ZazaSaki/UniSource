466d1d0c142c440f7f34fcaf58e47f7c85b9b353
ZazaSaki <isac.cruzm@gmail.com>
2024-04-29 17:36:30 +0100

    Testing display funtion

--- ACII/Aula 4/Extra_Creations/DisplayClockTest/Porg1.c
+++ ACII/Aula 4/Extra_Creations/DisplayClockTest/Porg1.c
@@ -32,18 +32,44 @@ void UpdateDefaults(){
     TRISE = 0xFF00;
     TRISB = 0X80FF;
     TRISDbits.TRISD8 = 1;
-    TRISDbits.TRISD5 = 0;
+    TRISDbits.TRISD5 = 1;
     TRISDbits.TRISD6 = 0;
     LATE = 0X0000;
     
+    //Way 3
+    setDispVal(45);
+    
+    //Way 1
+    int ch1 = DispLN(4);
+    int ch2 = DispLN(5);
+
     while (1)
     {   
         
-        int i = (PORTB & 0x000F)<<4;
-        Dysp(0,DispL('4'));
+        int i = (PORTB & 0x000F)<<8;
+        
+
+        
+        //Way 1
+        // Dysp(0,ch1);
+        // delay(i);
+        // Dysp(1,ch2);
+        // delay(i);
+        
+        
+        //Way 2
+        //Dysp10(0, 45);
+        // delay(i);
+        //Dysp10(1,45);
+        // delay(i);
+        
+        //Way 3
+        displayToggle();
         delay(i);
-        Dysp(1,DispL('5'));
+        displayToggle();
         delay(i);
+       
+
 
 
         
